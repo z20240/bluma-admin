c6b113794ebcb2ac7f52d326c83394e442de8fa6
les <les.lee@slotfactory.com>
2020-08-13 19:20:23 +0800

    temp

--- client/src/views/Questionnaire/index.vue
+++ client/src/views/Questionnaire/index.vue
@@ -1,23 +1,15 @@
 <template>
     <div class="main">
         <nav class="navbar">
-            <div class="navbar-brand">
+            <!-- <div class="navbar-right">
                 <a class="navbar-item" href="https://bulma.io">
-                    <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28" />
-                </a>
-
-                <a
-                    role="button"
-                    class="navbar-burger burger"
-                    aria-label="menu"
-                    aria-expanded="false"
-                    data-target="navbarBasicExample"
-                >
-                    <span aria-hidden="true"></span>
-                    <span aria-hidden="true"></span>
-                    <span aria-hidden="true"></span>
+                    <img src="@/assets/images/logo.png" width="112" height="100%" />
                 </a>
             </div>
+
+            <div class="navbar-left navbar-slogan">
+                『防治藥物濫用中心．關心您!』
+            </div>-->
         </nav>
 
         <section class="main-content hero is-fullheight-with-navbar">This is content</section>
@@ -42,6 +34,7 @@
 
 <script>
 import { onMounted } from '@vue/composition-api';
+// import LogoImg from '@/assets/images/logo.png';
 
 const themeClassList = [
     'has-aside-left',
@@ -51,6 +44,9 @@ const themeClassList = [
 
 export default {
     name: 'Questionnaire',
+    components: {
+        // LogoImg
+    },
     setup() {
         onMounted(() => {
             document.documentElement.classList.remove(...themeClassList);
@@ -63,6 +59,59 @@ export default {
 
 <style lang="scss" scoped>
 .main {
+    .navbar {
+        background: url("~@/assets/images/logo.png") no-repeat center center
+            fixed;
+        background-size: cover;
+
+        position: relative;
+        background-color: orange;
+        display: flex;
+        justify-content: space-between;
+
+        .navbar-right {
+            position: relative;
+            width: 50%;
+            @media screen and (min-width: 414px) {
+                width: 40%;
+            }
+
+            background-color: white;
+
+            .navbar-item {
+                position: relative;
+                box-sizing: border-box;
+                width: 100%;
+                z-index: 1;
+                &::after {
+                    position: absolute;
+                    right: -1px;
+                    top: 0;
+                    z-index: -1;
+                    content: " ";
+                    height: 100%;
+                    width: 0px;
+                    border-color: transparent orange white transparent;
+                    border-style: solid solid solid solid;
+                    border-width: 0px 52px 52px 0px;
+                }
+            }
+        }
+
+        .navbar-left {
+            &.navbar-slogan {
+                display: flex;
+                justify-content: flex-end;
+                align-items: center;
+
+                color: white;
+                font-weight: 600;
+
+                font-size: 1rem;
+            }
+        }
+    }
+
     .main-content {
         max-height: calc(100vh - 3.25rem);
         overflow: auto;
