bb098e8bf9987124b32849042ad66abe9a30d65e
z20240 <z20240z@gmail.com>
2020-08-25 00:08:01 +0800

    footer

--- client/src/views/Questionnaire/Footer.vue
+++ client/src/views/Questionnaire/Footer.vue
@@ -1,11 +1,11 @@
 <template>
-  <footer class="footer">
-    <div class="content has-text-centered">
-      <p>指導單位 | 教育部學生事務教育司．承辦單位 | 中正大學犯罪研究中心．反毒專線 | 0800-770885(請請您，幫幫我)</p>
-      <hr class="footer-hr" />
-      <p>Copyright © 2020防治藥物濫用教育中心NCC all righrs teserved.</p>
-    </div>
-  </footer>
+    <footer class="footer">
+        <div class="content has-text-centered">
+            <p>指導單位 | 教育部學生事務教育司．承辦單位 | 中正大學犯罪研究中心．反毒專線 | 0800-770885(請請您，幫幫我)</p>
+            <br />
+            <p>Copyright © 2020防治藥物濫用教育中心NCC all righrs teserved.</p>
+        </div>
+    </footer>
 </template>
 
 <script>
@@ -16,22 +16,22 @@ export default {
 
 <style  lang="scss" scoped>
 footer {
-  @media screen and (min-width: 769px) {
-    position: fixed;
-    bottom: 0;
-  }
-  width: 100%;
-  color: #fff;
-  background-color: rgb(116, 86, 4);
+    @media screen and (min-width: 769px) {
+        position: fixed;
+        bottom: 0;
+    }
+    width: 100%;
+    color: #fff;
+    background-color: rgb(116, 86, 4);
 
-  .content {
-    p {
-      margin: 0;
-      font-size: 0.75rem;
+    .content {
+        p {
+            margin: 0;
+            font-size: 0.75rem;
+        }
+        .footer-hr {
+            margin: 0;
+        }
     }
-    .footer-hr {
-      margin: 0;
-    }
-  }
 }
 </style>
