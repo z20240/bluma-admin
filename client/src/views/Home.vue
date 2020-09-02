2fe7dec7e366a3d3fc98104e04b082ce6e1c6b9e
z20240 <z20240z@gmail.com>
2020-09-03 00:00:30 +0800

    change the charyt

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,24 +1,9 @@
 <template>
     <section class="section">
-        {{ correctRateByEachQuestions }}
-        {{ Array.from(new Set(logs.flatMap(log => log.details).map(m => m.id))) }}
         <title-bar>歡迎, {{ userName }}!</title-bar>
         <tiles>
-            <card-widget
-                class="tile is-child"
-                type="is-primary"
-                icon="frequently-asked-questions"
-                :number="logs.length"
-                label="總答題數"
-            />
-            <card-widget
-                class="tile is-child"
-                type="is-info"
-                icon="percent-outline"
-                :number="avgCorrectRate"
-                suffix="%"
-                label="答題正確率"
-            />
+            <card-widget class="tile is-child" type="is-primary" icon="frequently-asked-questions" :number="logs.length" label="總樣本數" />
+            <card-widget class="tile is-child" type="is-info" icon="percent-outline" :number="avgCorrectRate" suffix="%" label="答題正確率" />
         </tiles>
 
         <card-component title="各題目答對率" icon="finance">
@@ -33,12 +18,7 @@
             </div>
         </card-component>
 
-        <card-component
-            title="Performance"
-            @header-icon-click="fillChartData"
-            icon="finance"
-            header-icon="reload"
-        >
+        <card-component title="Performance" @header-icon-click="fillChartData" icon="finance" header-icon="reload">
             <div v-if="defaultChart.chartData" class="chart-area">
                 <line-chart
                     style="height: 100%"
@@ -48,13 +28,6 @@
                     :extra-options="defaultChart.extraOptions"
                 ></line-chart>
             </div>
-        </card-component>
-
-        <card-component title="Clients" class="has-table">
-            <clients-table-sample
-                :data-url="`${$router.options.base}data-sources/clients.json`"
-                :checkable="true"
-            />
         </card-component>
     </section>
 </template>
@@ -70,14 +43,12 @@ import Tiles from '@/components/Tiles';
 import CardWidget from '@/components/CardWidget';
 import BarChart from '@/components/Charts/BarChart';
 import LineChart from '@/components/Charts/LineChart';
-import ClientsTableSample from '@/components/ClientsTableSample';
 import TitleBar from '@/components/TitleBar';
 
 export default {
     name: 'home',
     components: {
         TitleBar,
-        ClientsTableSample,
         CardWidget,
         Tiles,
         CardComponent,
@@ -108,21 +79,14 @@ export default {
             const rate = this.logs.reduce((pre, cur) => pre + cur.score, 0) / (this.logs.length * 100);
             return Math.round(rate * 10000) / 100;
         },
-        questionIDs() {
-            return this.questions.map(question => question.id);
-        },
         questionMap() {
             return this.questions.reduce((prev, question) => ({
                 ...prev,
                 [question.id]: question
             }), {});
         },
-        correctRateByEachQuestions() {
-            const roundTo2 = (x, y) => Math.round(Number(x) / Number(y) * 10000) / 100;
-
-            // 這邊改成只顯示有被答題過的
-
-            const correctQuestionsMap = this.logs
+        correctQuestionsMap() {
+            return this.logs
                 .flatMap(log => log.details)
                 .reduce((prev, {id, correct}) => ({
                     ...prev,
@@ -131,9 +95,12 @@ export default {
                         correct: prev[id] ? prev[id].correct + Number(!!correct) : Number(!!correct)
                     },
                 }), {});
+        },
+        correctRateByEachQuestions() {
+            const roundTo2 = (x, y) => Math.round(Number(x) / Number(y) * 10000) / 100;
 
-            return this.questionIDs.map(id => {
-                const record = correctQuestionsMap[id];
+            return Object.keys(this.correctQuestionsMap).map(id => {
+                const record = this.correctQuestionsMap[id];
                 return record ? roundTo2(record.correct, record.count) : 0;
             });
         },
@@ -148,7 +115,7 @@ export default {
                         data: this.correctRateByEachQuestions
                     }
                 ],
-                labels: this.questionIDs // 這邊要跟著動
+                labels: Object.keys(this.correctQuestionsMap)
             };
         },
         barChartOptions() {
@@ -162,8 +129,7 @@ export default {
                         title: function([tooltipItem]) {
                             return `題 ${tooltipItem.label}：${self.questionMap[tooltipItem.label].question}`;
                         },
-                        label: function(tooltipItem, data) {
-                            console.log("barChartOptions -> tooltipItem, data", tooltipItem, data);
+                        label: function(tooltipItem) {
                             return `答對率：${tooltipItem.value}%`;
                         },
                     },
@@ -196,7 +162,6 @@ export default {
             try {
                 const { data } = await apiGetHistory(this.queryData);
                 this.logs = data.Data;
-                console.log("getHistory -> data", this.logs);
             } catch (err) {
                 console.error("getHistory -> err", err);
                 this.$buefy.snackbar.open({
@@ -210,7 +175,6 @@ export default {
             try {
                 const { data } = await apiGetAllQuestion();
                 this.questions = data.Data;
-                console.log("getAllQuestions -> data", this.questions);
             } catch (err) {
                 console.error("getAllQuestions -> err", err);
                 this.$buefy.snackbar.open({
