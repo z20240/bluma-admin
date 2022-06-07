b4291c1b1fd4c618da88f4a284af779c5b336079
les <z20240z@gmail.com>
2022-06-07 17:50:17 +0800

    update

--- server/database/mongodb.js
+++ server/database/mongodb.js
@@ -1,61 +1,58 @@
 /** @format */
-var promisify = require('util').promisify;
-const MongoClient = require('mongodb').MongoClient;
-const url = `mongodb://${process.env.DATABASE_HOST}:${process.env.DATABASE_PORT}`;
+var promisify = require("util").promisify;
+const MongoClient = require("mongodb").MongoClient;
+const url = `mongodb://${process.env.MONGO_INITDB_ROOT_USERNAME}:${process.env.MONGO_INITDB_ROOT_PASSWORD}@${process.env.DATABASE_HOST}:${process.env.DATABASE_PORT}`;
+console.log("🚀 ~ file: mongodb.js ~ line 5 ~ url", url);
 
 const CreateDB = async function (db_name, app = null) {
-    // Create a MongoDB connection pool and start the application
-    // after the database connection is ready
-    return new Promise((resolve, reject) => {
-        MongoClient.connect(
-            url,
-            {
-                promiseLibrary: Promise,
-                useNewUrlParser: true,
-                useUnifiedTopology: true,
-            },
-            (err, db) => {
-                if (err) {
-                    console.error(
-                        `Failed to connect to the database. ${err.stack}`
-                    );
-                    reject(err);
-                }
+  // Create a MongoDB connection pool and start the application
+  // after the database connection is ready
+  return new Promise((resolve, reject) => {
+    MongoClient.connect(
+      url,
+      {
+        promiseLibrary: Promise,
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      },
+      (err, db) => {
+        if (err) {
+          console.error(`Failed to connect to the database. ${err.stack}`);
+          reject(err);
+        }
 
-                if (app) app.locals.db = db.db(db_name);
+        if (app) app.locals.db = db.db(db_name);
 
-                resolve(db.db(db_name));
-            }
-        );
-    });
+        resolve(db.db(db_name));
+      }
+    );
+  });
 };
 
 const GetSequenceNextValue = async (tableName) => {
-    const db = await CreateDB(process.env.DATABASE_DB);
-    const db_collection = db.collection('autoIncrease');
+  const db = await CreateDB(process.env.DATABASE_DB);
+  const db_collection = db.collection("autoIncrease");
 
-    const dbFindOneAndUpdateAsync = promisify(
-        db_collection.findOneAndUpdate
-    ).bind(db_collection);
+  const dbFindOneAndUpdateAsync = promisify(db_collection.findOneAndUpdate).bind(db_collection);
 
-    const result = await dbFindOneAndUpdateAsync(
-        { _id: tableName },
-        { $inc: { seqValue: 1 } },
-        { upsert: true, returnNewDocument: true }
-    );
+  const result = await dbFindOneAndUpdateAsync(
+    { _id: tableName },
+    { $inc: { seqValue: 1 } },
+    { upsert: true, returnNewDocument: true }
+  );
 
-    const seqDoc = result.value;
+  const seqDoc = result.value;
 
-    return seqDoc ? seqDoc.seqValue : 0;
+  return seqDoc ? seqDoc.seqValue : 0;
 };
 
 module.exports.Database = {
-    name: process.env.DATABASE_DB,
-    tables: {
-        User: 'user',
-        Question: 'question',
-        Log: 'log'
-    },
+  name: process.env.DATABASE_DB,
+  tables: {
+    User: "user",
+    Question: "question",
+    Log: "log",
+  },
 };
 
 module.exports.CreateDB = CreateDB;
